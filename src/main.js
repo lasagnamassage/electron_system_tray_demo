e850633285e81f9d973ad9712aa57f7c83dbc2ff
ashaunthomas <ashaunthomas@siu.edu>
2017-09-28 19:41:17 -0500

    added icon, fixed readme, app runs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 const electron = require('electron');
-const app = electron.app;
+const path = require('path');
+const { app, Tray, Menu } = electron;
 
 app.on('ready', _ => {
-    console.log('hello');
-})
+    const tray = new Tray(path.join("src","icon.png"));
+    const contextMenu = Menu.buildFromTemplate([
+        { 
+            label: 'Wow',
+            click: _ => console.log('wow')
+        },
+        {
+            label:'Awesome',
+            click: _ => console.log("awesome")
+        }
+    ])
+    tray.setContextMenu(contextMenu);
+});
